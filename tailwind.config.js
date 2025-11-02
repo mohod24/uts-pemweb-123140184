7d1cc5dc47b508855c7daa67d09b810efa37a747
aliefathallah <mohdmusyaffa.123140184@student.itera.ac.id>
2025-11-02 12:08:56 +0700

    dependencies part 2

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}", // <-- Pastikan ini sesuai dengan struktur proyek Anda
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
